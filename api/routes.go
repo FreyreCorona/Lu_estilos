ecc91e0f1dc10803c11deb8d31b9a23121f27413
FreyreCorona <einierfreyre60@gmail.com>
2025-08-02 21:23:32 -0300

    hello world

--- api/routes.go
+++ api/routes.go
@@ -1,3 +1,10 @@
 package api
 
-//
+import (
+	"fmt"
+	"net/http"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo")
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/FreyreCorona/Lu_estilos/api"
+)
+
 func main() {
+	Infolog := log.New(os.Stdout, "INFO: ", log.Ltime|log.Ldate)
+	Errorlog := log.New(os.Stdout, "ERROR: ", log.Ltime|log.Ldate|log.Lshortfile)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", api.Handle)
+
+	server := http.Server{
+		Addr:     ":8000",
+		Handler:  mux,
+		ErrorLog: Errorlog,
+	}
+	Infolog.Println("Starting Server...")
+	Errorlog.Fatal(server.ListenAndServe())
 }
