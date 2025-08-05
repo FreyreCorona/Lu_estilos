d0bc66ada7e4b7d803f55f816681c527a423ddfb
FreyreCorona <einierfreyre60@gmail.com>
2025-08-05 09:07:16 -0300

    add variable port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/FreyreCorona/Lu_estilos/internal/models"
 )
 
+type configuration struct {
+	port int
+}
 type application struct {
+	Config     configuration
 	InfoLogger *log.Logger
 	Clients    *models.ClietModel
 }
 
 func main() {
-	Infolog := log.New(os.Stdout, "INFO: ", log.Ltime|log.Ldate)
+	var cfg configuration
+
+	flag.IntVar(&cfg.port, "port", 4000, "Port listen number")
+	flag.Parse()
+	Infolog := log.New(os.Stdout, "", log.Ltime|log.Ldate)
 
 	app := application{
+		Config:     cfg,
 		InfoLogger: Infolog,
 	}
 
 	server := &http.Server{
-		Addr:    ":8000",
-		Handler: app.routes(),
+		Addr:         fmt.Sprintf(":%d", app.Config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
-	Infolog.Println("Starting Server...")
+	Infolog.Printf("Starting Server at %s ...\n", server.Addr)
 	log.Fatal(server.ListenAndServe())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/FreyreCorona/Lu_estilos
 
 go 1.24.5
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
