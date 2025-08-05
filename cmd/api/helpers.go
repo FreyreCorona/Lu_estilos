8ae27394816c70fa57a9615c20f1125e80ff888b
FreyreCorona <einierfreyre60@gmail.com>
2025-08-05 14:34:10 -0300

    create helper for writeJSON responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"maps"
 	"net/http"
 	"strconv"
 
@@ -15,4 +17,18 @@ func (app *application) readParamID(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	json, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	json = append(json, '\n')
+	w.WriteHeader(status)
+	maps.Copy(w.Header(), headers)
+	w.Header().Set("Content-Type", "application-json")
+	w.Write(json)
+
+	return nil
 }
