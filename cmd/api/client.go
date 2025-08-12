2c2958feaf6d3088c88e485d6f4abe7b4ea04615
FreyreCorona <einierfreyre60@gmail.com>
2025-08-12 09:12:00 -0300

    delete endpoint

--- cmd/api/client.go
+++ cmd/api/client.go
@@ -132,4 +132,24 @@ func (app *application) putClient(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) deleteClient(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readParamID(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	err = app.Models.Clients.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrNoRecord):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "client successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
