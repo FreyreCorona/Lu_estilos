097bd95911ab932c66e4c84425c26c96ccaa9298
FreyreCorona <einierfreyre60@gmail.com>
2025-08-14 15:42:17 -0300

    get route for products

--- cmd/api/product.go
+++ cmd/api/product.go
@@ -8,6 +8,27 @@ import (
 )
 
 func (app *application) getProductByID(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readParamID(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	product, err := app.Models.Products.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrNoRecord):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"product": product}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 }
 
 func (app *application) postProduct(w http.ResponseWriter, r *http.Request) {
